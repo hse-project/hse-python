05685926b43f54078907a4e064bf259579103fdb
Tristan Partin <tpartin@micron.com>
2020-11-26 12:26:49 -0600

    Reorganize test_params

--- test/test_params.py
+++ test/test_params.py
@@ -3,25 +3,19 @@ import os
 
 
 def test_get_set():
-    p = hse.Params()
-
-    p.set("kvs.pfx_len", "3")
+    p = hse.Params().set("kvs.pfx_len", "3")
 
     assert p.get("kvs.pfx_len") == "3"
 
 
 def test_from_string():
-    p = hse.Params()
-
-    p.from_string("api_version: 1\nkvs:\n  pfx_len: 3")
+    p = hse.Params().from_string("api_version: 1\nkvs:\n  pfx_len: 3")
 
     assert p.get("kvs.pfx_len") == "3"
 
 
 def test_from_file():
-    p = hse.Params()
-
-    p.from_file(
+    p = hse.Params().from_file(
         str(os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.yaml"))
     )
 
