4892fe466aaa57416d936946ce85ddf561c6ee10
Tristan Partin <tpartin@micron.com>
2020-12-07 11:55:34 -0600

    Skip compaction test for now

--- test/test_kvdb.py
+++ test/test_kvdb.py
@@ -1,4 +1,26 @@
 import hse
+import pytest
+from typing import Generator
+import errno
+
+
+@pytest.fixture(scope="module")
+def kvs(kvdb: hse.Kvdb) -> Generator[hse.Kvs, None, None]:
+    p = hse.Params().set("kvs.pfx_len", "3")
+
+    try:
+        kvdb.kvs_make("kvdb-test", params=p)
+    except hse.KvdbException as e:
+        if e.returncode == errno.EEXIST:
+            pass
+        else:
+            raise e
+    kvs = kvdb.kvs_open("kvdb-test", params=p)
+
+    yield kvs
+
+    kvs.close()
+    kvdb.kvs_drop("kvdb-test")
 
 
 def test_sync(kvdb: hse.Kvdb):
@@ -9,10 +31,13 @@ def test_flush(kvdb: hse.Kvdb):
     kvdb.flush()
 
 
-# def test_compact(kvdb: hse.Kvdb):
-#     kvdb.compact()
-#     status = kvdb.compact_status
-#     assert status.active
-#     kvdb.compact(cancel=True)
-#     status = kvdb.compact_status
-#     assert status.canceled
+@pytest.mark.skip(reason="Hard to control when compaction occurs")
+def test_compact(kvdb: hse.Kvdb, kvs: hse.Kvs):
+    for i in range(1000):
+        kvs.put(f"key{i}".encode(), f"value{i}".encode())
+    kvdb.compact()
+    status = kvdb.compact_status
+    assert status.active
+    kvdb.compact(cancel=True)
+    status = kvdb.compact_status
+    assert status.canceled
